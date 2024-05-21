4c27ec56230c1fcf64e984a170cebbfb411c223b
StrekozovVA <strekozov_VA@hotmail.com>
2024-05-21 14:51:21 +0300

    m1

--- src/test/java/plaindoll/WelcomerTest.java
+++ src/test/java/plaindoll/WelcomerTest.java
@@ -30,8 +30,4 @@ public class WelcomerTest {
 	public void welcomerSaysSomething(){
 		assertThat(welcomer.saySome(), containsString("something"));
 	}
-	@Test
-	public void welcomerHeadHunter() {
-		assertThat(welcomer.HeadHunter(), containsString("hunter"));
-	}	
 }
